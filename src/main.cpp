eb363679339f946c6faa8bb12b6a5c45b1d939a8
the-kim-jong-un <60155949+the-kim-jong-un@users.noreply.github.com>
2020-06-29 10:21:43 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "Convert.h"
 
 int main() {
-    Convert::grayscaleImage("planet.jpg", "p.jpg", 0, 1080, 0, 1351,0.2);
-    Convert::grayscaleImage("planet.jpg", "tttt.png", 0, 1080, 0, 1351,3);
+    Convert::grayscaleImage("planet.jpg", "planet_2.jpg", 0, 500, 0, 500, 1);
     return 0;
 }
